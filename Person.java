97de6a229db3f383c58571697e581d9137d02518
nandanaa847 <mohangowda0727@gmail.com>
2024-11-13 17:33:25 +0530

    Successfully added

---
+++ Person.java
@@ -0,0 +1,41 @@
+public class Person{
+	
+	public static void main(String[] args){
+
+	short[] pagenumber = {196,981,195,200,100,199,198,85,174,18};
+	
+	short[] yearofbirth = new short[10];
+	yearofbirth[0]= 2006;
+	yearofbirth[1]= 2002;
+	yearofbirth[2]= 2024;
+	yearofbirth[3]= 1947;
+	yearofbirth[4]= 1987;
+	yearofbirth[5]= 1976;
+	yearofbirth[6]= 1989;
+	yearofbirth[7]= 2017;
+	yearofbirth[8]= 1983;
+	yearofbirth[9]= 1999;
+	
+	System.out.println(yearofbirth[0]);
+	System.out.println(yearofbirth[1]);
+	System.out.println(yearofbirth[2]);
+	System.out.println(yearofbirth[3]);
+	System.out.println(yearofbirth[4]);
+	System.out.println(yearofbirth[5]);
+	System.out.println(yearofbirth[6]);
+	System.out.println(yearofbirth[7]);
+	System.out.println(yearofbirth[8]);
+	System.out.println(yearofbirth[9]);
+
+	System.out.println(pagenumber[0]);
+	System.out.println(pagenumber[1]);
+	System.out.println(pagenumber[2]);
+	System.out.println(pagenumber[3]);
+	System.out.println(pagenumber[4]);
+	System.out.println(pagenumber[5]);
+	System.out.println(pagenumber[6]);
+	System.out.println(pagenumber[7]);
+	System.out.println(pagenumber[8]);
+	System.out.println(pagenumber[9]);
+	}
+	}
