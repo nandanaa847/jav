55241d01f09e04e404ad281e48a207b57f30e2c8
nandanaa847 <mohangowda0727@gmail.com>
2024-11-13 17:52:49 +0530

    Successfully added

---
+++ Account.java
@@ -0,0 +1,43 @@
+public class Account{
+	
+	public static void main(String[] args){
+
+	double[] litres = {4.19,20.24,62.2,24.24,19.81,19.14,71.94,58.11,18.92,23,67};
+
+	double[] temperature = new double[10];
+	temperature[0]= 20.13;
+	temperature[1]= 20.62;
+	temperature[1]= 16.25;
+	temperature[2]= 74.83;
+	temperature[3]= 35.65;
+	temperature[4]= 36.65;
+	temperature[5]= 42.52;
+	temperature[6]= 48.23;
+	temperature[7]= 32.76;
+	temperature[8]= 28.26;
+	temperature[9]= 36.27;
+
+	
+	System.out.println(temperature[0]);
+	System.out.println(temperature[1]);
+	System.out.println(temperature[2]);
+	System.out.println(temperature[3]);
+	System.out.println(temperature[4]);
+	System.out.println(temperature[5]);
+	System.out.println(temperature[6]);
+	System.out.println(temperature[7]);
+	System.out.println(temperature[8]);
+	System.out.println(temperature[9]);
+
+	System.out.println(litres[0]);
+	System.out.println(litres[1]);
+	System.out.println(litres[2]);
+	System.out.println(litres[3]);
+	System.out.println(litres[4]);
+	System.out.println(litres[5]);
+	System.out.println(litres[6]);
+	System.out.println(litres[7]);
+	System.out.println(litres[8]);
+	System.out.println(litres[9]);
+	}
+	}
