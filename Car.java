8de2847393de2e5ab7c5133898d23b52f7936900
nandanaa847 <mohangowda0727@gmail.com>
2024-11-13 17:48:22 +0530

    Successfully added

---
+++ Car.java
@@ -0,0 +1,41 @@
+public class Car{
+	
+	public static void main(String[] args){
+
+	long[] accountbalance = {25430000,14720000,42500000,38500000,34100000,26700000,26300000,22400000,21600000,20400000};
+
+	long[] gdpofcountries = new long[10];
+	gdpofcountries[0]= 19200000;
+	gdpofcountries[1]= 16900000;
+	gdpofcountries[2]= 23930000;
+	gdpofcountries[3]= 16700000;
+	gdpofcountries[4]= 14600000;
+	gdpofcountries[5]= 14100000;
+	gdpofcountries[6]= 22400000;
+	gdpofcountries[7]= 33500000;
+	gdpofcountries[8]= 18530000;
+	gdpofcountries[9]= 35290000;
+	
+	System.out.println(gdpofcountries[0]);
+	System.out.println(gdpofcountries[1]);
+	System.out.println(gdpofcountries[2]);
+	System.out.println(gdpofcountries[3]);
+	System.out.println(gdpofcountries[4]);
+	System.out.println(gdpofcountries[5]);
+	System.out.println(gdpofcountries[6]);
+	System.out.println(gdpofcountries[7]);
+	System.out.println(gdpofcountries[8]);
+	System.out.println(gdpofcountries[9]);
+
+	System.out.println(accountbalance[0]);
+	System.out.println(accountbalance[1]);
+	System.out.println(accountbalance[2]);
+	System.out.println(accountbalance[3]);
+	System.out.println(accountbalance[4]);
+	System.out.println(accountbalance[5]);
+	System.out.println(accountbalance[6]);
+	System.out.println(accountbalance[7]);
+	System.out.println(accountbalance[8]);
+	System.out.println(accountbalance[9]);
+	}
+	}
