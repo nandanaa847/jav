5e0a70d59d4c0fa84c279e846b2c18335e26b73c
nandanaa847 <mohangowda0727@gmail.com>
2024-11-19 18:08:38 +0530

    Successfully added

---
+++ Movie.java
@@ -0,0 +1,44 @@
+ class Movie{
+	
+	String movieName;
+	String genre;
+	String duration;
+	String language;
+	int releaseYear;
+    float rating;
+
+
+public static void play(){
+    	System.out.println("Start playing the movie");
+    }
+     public static void pause(){
+    	System.out.println("pause the movie for a minute");
+    }
+     public static void stop(){
+    	System.out.println("Stop playing");
+    }
+    public static void details(){
+    	System.out.println("Display the movie details");
+    }
+    public static void messageofraajakumara(){
+    	System.out.println("Father and son emotions");
+    }
+    public static void success(){
+    	System.out.println("Money,Awards,Fanfollowing");
+    }
+    public static void messageoftraintobusan(){
+    	System.out.println("sacrifice");
+    }
+     public static void messageof777charlie(){
+        System.out.println("Human and animal relationship");
+    }
+    public static void messageofkirikparty(){
+        System.out.println("Friendship,college days");
+    }
+    public static void messageofgoogly(){
+        System.out.println("Friendship,college days");
+    }
+   
+   
+   
+}
