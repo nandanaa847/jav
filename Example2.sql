2945302cc0224a15a8c578880204a48f56058fc0
nandanaa847 <mohangowda0727@gmail.com>
2024-12-26 18:31:13 +0530

    Successfully added

---
+++ Example2.sql
@@ -0,0 +1,14 @@
+create database student;
+
+
+use student;
+
+create table studetails(name varchar(20),
+weight float,
+height double,
+sub_code char(10),
+stu_id bigint,
+admission_date date,
+age int);
+
+desc studetails;
