ca2500257085de9f23093d03c0a0654520329237
nandanaa847 <nandana200419@gmail.com>
2025-01-04 16:05:50 +0530

    Update DDL.sql

--- DDL.sql
+++ DDL.sql
@@ -18,6 +18,23 @@ use watch;
 
 rename table watchdetails to watchinformation;
 
+create database bankaccount;
+
+use bankaccount;
+
+create table accountdetails(account_holder_name varchar(10),
+ifsc_code char(10),
+balance double,
+UPI_ID bigint,
+mobile_num bigint,
+opening_date date,
+age int,
+account_type varchar(7),
+account_ID char(10),
+transaction_limit int);
+
+desc accountdetails;
+
 use music;
 
 rename table musicdetils to songdetails;
