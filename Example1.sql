ded11375b5e0f5db0e769e7c0e19b674ac5a2b2a
nandanaa847 <nandana200419@gmail.com>
2025-01-09 13:45:06 +0530

    Update Example1.sql

--- Example1.sql
+++ Example1.sql
@@ -24,3 +24,17 @@ admission_date date,
 age int);
 
 desc studetails;
+
+
+create database orders;
+use orders;
+
+create table saree(type varchar(20),
+length float,
+width double,
+order_code char(10),
+product_ID bigint,
+delivery_date date,
+quantity int);
+
+desc saree;
