69aff0c1fddd2ba2232d372b87b6f4d3c8d3318b
nandanaa847 <mohangowda0727@gmail.com>
2025-01-27 18:23:39 +0530

    added

---
+++ DigiLockerService.java
@@ -0,0 +1,10 @@
+package com.xworkz.validate.service;
+
+import com.xworkz.validate.dto.DigiLockerDto;
+
+
+public abstract class DigiLockerService {
+
+	public abstract String validateAnddSave(DigiLockerDto digidto);
+
+}
