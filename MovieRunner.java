c43c2244dd06c7b6998de7eea4eb3eacf640ecbe
nandanaa847 <mohangowda0727@gmail.com>
2025-01-27 18:28:13 +0530

    added

---
+++ MovieRunner.java
@@ -0,0 +1,33 @@
+package com.xworkz.validate.runner;
+
+import com.xworkz.validate.dto.MovieDto;
+import com.xworkz.validate.service.MovieService;
+import com.xworkz.validate.service.MovieServiceImple;
+
+public class MovieRunner {
+
+public static void main(String[] args) {
+	
+	MovieDto moviedto = new MovieDto();
+	
+	moviedto.setActorage(40);
+	moviedto.setActorname("yash");
+	moviedto.setActressname("kriti");
+	moviedto.setBudget(364737776);
+	moviedto.setDirectorname("pavan wadiyar");
+	moviedto.setDuration(142);	
+	moviedto.setGenre("comedy");
+	moviedto.setLanguage("kannada");
+	moviedto.setProducername("jayanna");
+	moviedto.setRating(9.5f);
+	moviedto.setReleasedate("january 2013");
+	moviedto.setReview("Goodd");
+	moviedto.setTicketprice(250);
+	moviedto.setTotalcollection(365365638);
+	
+	System.out.println(moviedto);
+	
+	MovieService movieservice = new MovieServiceImple();
+movieservice.validateAnddSave(moviedto);
+	}
+}
