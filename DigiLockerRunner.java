c44f1883663edade936d357368f44eae040f4b17
nandanaa847 <mohangowda0727@gmail.com>
2025-01-27 18:30:03 +0530

    added

---
+++ DigiLockerRunner.java
@@ -0,0 +1,30 @@
+package com.xworkz.validate.runner;
+
+import com.xworkz.validate.dto.DigiLockerDto;
+import com.xworkz.validate.service.DigiLockerService;
+import com.xworkz.validate.service.DigiLockerServiceImple;
+
+public class DigiLockerRunner {
+	
+public static void main(String[] args) {
+	
+	DigiLockerDto digilockerdto = new DigiLockerDto();
+	
+	digilockerdto.setDocID(1342);
+	digilockerdto.setDocname("thiss");
+	digilockerdto.setDocname("pdff");
+	digilockerdto.setLockerID(1234);
+	digilockerdto.setLockername("npch");
+	digilockerdto.setLockerstatus("locked");
+	digilockerdto.setPassword("chpona");
+	digilockerdto.setPasswordcapacity(10);
+	digilockerdto.setUserID(4567);
+	digilockerdto.setUsername("chinmaye");
+	
+	System.out.println(digilockerdto);
+	
+	DigiLockerService digilockerservice = new DigiLockerServiceImple(); 
+	digilockerservice.validateAnddSave(digilockerdto);
+	}
+
+}
