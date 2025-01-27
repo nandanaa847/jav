5ff4cfa9f35fa64972db3bcaf5c0d4182bc8a0e2
nandanaa847 <mohangowda0727@gmail.com>
2025-01-27 18:03:00 +0530

    added

---
+++ FitnessCenterService.java
@@ -0,0 +1,9 @@
+package com.xworkz.validate.service;
+
+import com.xworkz.validate.dto.FitnessCenterDto;
+
+public abstract class FitnessCenterService {
+	
+	public abstract String validateAndSave(FitnessCenterDto fitdto);
+
+}
