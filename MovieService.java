5e4a1f96de55f8334be99513203fa22fd186c844
nandanaa847 <mohangowda0727@gmail.com>
2025-01-27 18:22:42 +0530

    added

---
+++ MovieService.java
@@ -0,0 +1,8 @@
+package com.xworkz.validate.service;
+
+import com.xworkz.validate.dto.MovieDto;
+
+public abstract class MovieService {
+
+	public abstract String validateAnddSave(MovieDto moviedto);
+}
