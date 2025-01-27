11884b8ff3bd0accbe7c5337d7261353fb5280ad
nandanaa847 <mohangowda0727@gmail.com>
2025-01-27 18:11:54 +0530

    added

---
+++ T20Service.java
@@ -0,0 +1,13 @@
+package com.xworkz.validate.service;
+
+
+import com.xworkz.validate.dto.T20Dto;
+
+public abstract class T20Service {
+	
+	public abstract String validateAnddSave(T20Dto dto);
+
+	
+	
+
+}
