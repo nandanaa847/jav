7cdaaaa00272822eddc11b800f14826e4f062d77
nandanaa847 <mohangowda0727@gmail.com>
2025-01-27 18:07:34 +0530

    added

---
+++ LibraryService.java
@@ -0,0 +1,9 @@
+package com.xworkz.validate.service;
+
+import com.xworkz.validate.dto.LibraryDto;
+
+public abstract class LibraryService {
+	
+	public abstract String validateAnddSave(LibraryDto dto);
+
+}
